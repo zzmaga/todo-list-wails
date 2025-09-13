48f22e233851400d0e59fa6fb4b1646e23186cd6
mtastan <magjantastanov@gmail.com>
2025-09-13 19:48:08 +0500

    temp usecase

---
+++ backend/usecase/task_usecase.go
@@ -0,0 +1,52 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"todo-list-wails/backend/models"
+	"todo-list-wails/backend/repo"
+
+	"github.com/google/uuid"
+)
+
+type TaskUsecase struct {
+	repo repo.TaskRepo
+}
+
+func NewTaskUsecase(r repo.TaskRepo) *TaskUsecase {
+	return &TaskUsecase{repo: r}
+}
+
+func (u *TaskUsecase) List(ctx context.Context) ([]models.Task, error) {
+	return u.repo.List(ctx)
+}
+
+func (u *TaskUsecase) Add(ctx context.Context, title string, due *time.Time, p models.Priority) (models.Task, error) {
+	t := models.Task{
+		ID:        uuid.NewString(),
+		Title:     title,
+		Done:      false,
+		CreatedAt: time.Now(),
+		DueAt:     due,
+		Priority:  p,
+	}
+	return t, u.repo.Add(ctx, t)
+}
+
+func (u *TaskUsecase) Toggle(ctx context.Context, id string) error {
+	t, err := u.repo.Get(ctx, id)
+	if err != nil {
+		return err
+	}
+	t.Done = !t.Done
+	return u.repo.Update(ctx, *t)
+}
+
+func (u *TaskUsecase) Update(ctx context.Context, t models.Task) error {
+	return u.repo.Update(ctx, t)
+}
+
+func (u *TaskUsecase) Delete(ctx context.Context, id string) error {
+	return u.repo.Delete(ctx, id)
+}
